b046a5f17160498a0e2b96295c44ca7f7163a2bd
BugHunter-X <askzenai@gmail.com>
2025-02-14 13:52:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+//This is a simple express server but it can be extended to create APIs, web pages etc.
